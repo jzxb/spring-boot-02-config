8df07328e4c22b6f7620c1aef1508b617aab74d6
jzxb <qq807572809@live.com>
2019-04-08 17:04:37 +0800

    Initial commit

---
+++ src/main/java/org/lhx/springboot/SpringBoot02ConfigApplication.java
@@ -0,0 +1,13 @@
+package org.lhx.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot02ConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot02ConfigApplication.class, args);
+    }
+
+}
---
+++ src/main/java/org/lhx/springboot/bean/Dog.java
@@ -0,0 +1,35 @@
+package org.lhx.springboot.bean;
+
+/**
+ * @author lhx
+ * @date 2019/4/8 - 16:45
+ */
+public class Dog {
+
+    private String name;
+    private Integer age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Dog{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
